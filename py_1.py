60f4469fdb404b0823ef12a5aeef6caaab57cdde
Sritilak-Sahu <sri782717@gmail.com>
2024-08-01 17:36:10 +0530

    Inserted values to collection

--- py_1.py
+++ py_1.py
@@ -1,9 +1,21 @@
 import pymongo
 try:
     client=pymongo.MongoClient('mongodb://localhost:27017')
-    db=client['Mongo-Db']
-    collection=db['Mongo-Db project']
-    print('Successfull',collection)
-   
+    db=client['project']
+    collection=db['project1']
+    # print('Successfull',collection)
+    
 except Exception as e:
     print("error",e)
+
+def create(name,age,city):
+    # db_insert=collection.({'name':name,'age':age,'city':city})
+    # print(db_insert,'Successful')
+    db_insert=collection.insert_one({'name':name,'age':age,'city':city})
+    print(db_insert,'Successful')
+
+
+def main():
+    create('Jio',21,'Rourkela')
+
+main()
